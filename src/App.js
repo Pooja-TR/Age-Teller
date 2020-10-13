a2d0d35b4a49b4e3bb001d1e3d4bc5d0f486b115
Pooja-TR <trpooja978@gmail.com>
2020-10-13 21:51:08 +0530

    First Commit of Age-Teller

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Form, FormControl, Button} from 'react-bootstrap';
 import './App.css';
+import AgeStats from './AgeStats'
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      newDate : '',
+      birthday : '1997-08-25',
+      //yyyy-mm-dd
+      showStats : false
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  changeBday(){
+    console.log(JSON.stringify(this.state));
+    this.setState({
+      birthday : this.state.newDate,
+      showStats : true
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <h1> AGE TELLER </h1>
+      <div>
+        <Form inline>
+          <h2>Pick your Birthday !!!</h2>
+          <FormControl 
+          type="date"
+          onChange = {event => this.setState({ newDate : event.target.value})}
+          ></FormControl>
+          {' '}
+          <Button
+          onClick = { () => this.changeBday()}
+          > Submit </Button>
+          {
+            this.state.showStats 
+            ? <div className="fade age-stats"><AgeStats date={this.state.birthday}/></div>
+            : <div></div>
+          }
+          
+        </Form>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
